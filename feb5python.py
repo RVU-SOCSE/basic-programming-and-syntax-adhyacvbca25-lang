4d61abb7941de54d409a1f47fc46d52abcd7dcab
adhyacvbca25-lang <adhyacvbca25@rvu.edu.in>
2026-02-05 10:22:54 +0530

    Add files via upload

---
+++ feb5python.py
@@ -0,0 +1,79 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+# simple calculator
+
+=============================================== RESTART: C:/Users/Akshay c v/AppData/Local/Programs/Python/Python314/simplecalculator.py ===============================================
+Enter first number: 3
+Enter second number: 4
+Enter operator (+, -, *, /): +
+Result: 7.0
+
+=============================================== RESTART: C:/Users/Akshay c v/AppData/Local/Programs/Python/Python314/simplecalculator.py ===============================================
+Enter first number: 3
+Enter second number: 7
+Enter operator (+, -, *, /): +
+Result: 10.0
+# temperature converter
+
+============================================= RESTART: C:/Users/Akshay c v/AppData/Local/Programs/Python/Python314/temperatureconverter.py =============================================
+Enter temperature in Celsius: 100
+Celsius to Fahrenheit: 212.0
+Enter temperature in Fahrenheit: 33
+Fahrenheit to Celsius: 0.5555555555555556
+
+==================================================== RESTART: C:/Users/Akshay c v/AppData/Local/Programs/Python/Python314/minmax.py ====================================================
+Traceback (most recent call last):
+  File "C:/Users/Akshay c v/AppData/Local/Programs/Python/Python314/minmax.py", line 1, in <module>
+    a=input(int("enter the first number: "))
+ValueError: invalid literal for int() with base 10: 'enter the first number: '
+
+==================================================== RESTART: C:/Users/Akshay c v/AppData/Local/Programs/Python/Python314/minmax.py ====================================================
+enter the first number: 2
+enter the second number: 5
+enter the third number: 3
+maximum value: 5
+minimum value: 2
+>>> minimum value: 2
+SyntaxError: invalid syntax
+>>> #simple calculator       1RUA25BCA0004
+>>> 
+=============================================== RESTART: C:/Users/Akshay c v/AppData/Local/Programs/Python/Python314/simplecalculator.py ===============================================
+Enter first number: 5
+Enter second number: 2
+Enter operator (+, -, *, /): -
+Result: 3.0
+>>> #temperature converter    1RUA25BCA0004
+>>> 
+============================================= RESTART: C:/Users/Akshay c v/AppData/Local/Programs/Python/Python314/temperatureconverter.py =============================================
+Enter temperature in Celsius: 98
+Celsius to Fahrenheit: 208.4
+Enter temperature in Fahrenheit: 32
+Fahrenheit to Celsius: 0.0
+>>> 
+============================================= RESTART: C:/Users/Akshay c v/AppData/Local/Programs/Python/Python314/temperatureconverter.py =============================================
+Enter temperature in Celsius: 100
+Celsius to Fahrenheit: 212.0
+Enter temperature in Fahrenheit: 32
+Fahrenheit to Celsius: 0.0
+>>> 
+============================================= RESTART: C:/Users/Akshay c v/AppData/Local/Programs/Python/Python314/temperatureconverter.py =============================================
+Enter temperature in Celsius: 100
+Celsius to Fahrenheit: 212.0
+Enter temperature in Fahrenheit: 30
+Fahrenheit to Celsius: -1.1111111111111112
+>>> 
+==================================================== RESTART: C:/Users/Akshay c v/AppData/Local/Programs/Python/Python314/minmax.py ====================================================
+enter the first number: 3
+enter the second number: 0
+enter the third number: 8
+maximum value: 8
+minimum value: 0
+>>> # max min value      1RUA25BCA0004
+>>> 
+==================================================== RESTART: C:/Users/Akshay c v/AppData/Local/Programs/Python/Python314/minmax.py ====================================================
+enter the first number: 2
+enter the second number: 3
+enter the third number: 1
+maximum value: 3
+minimum value: 1
+>>> #temperature converter
